f8c25da84f3ffc9300fb80f1b1a1791232f82b56
scube <scube@SCUBE>
2022-06-12 13:01:44 +0600

    Add project files.

---
+++ WebApp/Models/EmpJoinDateClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class EmpJoinDateClass
+    {
+        [Key]
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public string Email { get; set; }
+        public char Gender { get; set; }
+
+        public DateTime Joindate { get; set; }
+    }
+}
---
+++ WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public IEnumerable<EmpJoinDateClass> results { get; set; }
+
+        public void OnGet()
+        {
+
+            results = _db.NewEmployeeJoin.ToList();
+        }
+
+       public void OnPost(DateTime startdate, DateTime enddate)
+        {
+            results = (from x in _db.NewEmployeeJoin where(x.Joindate<=startdate)&&(x.Joindate>=enddate) select x).ToList();
+        }
+    }
+}
